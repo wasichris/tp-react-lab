812214943fa588a37179fe08748f9c227442f5ca
Chris Chen <wasi.chris@gmail.com>
2019-10-29 10:19:25 +0800

    加入 eslint 開發規範檢查

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+  env: {
+    browser: true, // browser global variables
+    es6: true, // enable all ECMAScript 6 features except for modules
+    jquery: true, // jQuery global variables
+    worker: true // web workers global variables
+  },
+  extends: [
+    'standard', // eslint-config-standard
+    'standard-react' // eslint-config-standard-react
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly'
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module'
+  },
+  plugins: [
+    'react',
+    'react-hooks' // rules for hooks (eslint-plugin-react-hooks)
+  ],
+  rules: {
+    "react-hooks/rules-of-hooks": "error",
+    "react-hooks/exhaustive-deps": "warn"
+  }
+}
