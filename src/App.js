3f39f8bd586040239fc7a9cae53b3ed4e552dce3
Chris Chen <wasi.chris@gmail.com>
2019-10-29 11:22:45 +0800

    使用 SCSS 作為樣式處理器

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import React from 'react'
-import logo from './logo.svg'
-import './App.css'
+import logo from './assets/images/logo.svg'
+import './assets/css/app.scss'
 
 function App () {
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
+    <div className='app'>
+      <header className='app-header'>
+        <img src={logo} className='app-logo' alt='logo' />
         <p>
             Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className='App-link'
+          className='app-link'
           href='https://reactjs.org'
           target='_blank'
           rel='noopener noreferrer'
--- src/index.js
+++ src/index.js
@@ -2,9 +2,8 @@ import 'react-app-polyfill/ie11'
 import 'react-app-polyfill/stable'
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './index.css'
 import App from './App'
-import * as serviceWorker from './serviceWorker'
+import * as serviceWorker from './utils/serviceWorker'
 
 ReactDOM.render(<App />, document.getElementById('root'))
 
