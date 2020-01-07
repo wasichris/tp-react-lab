9378baaf4b12aba314696fb51b635cf2bfc3e742
Chris Chen <wasi.chris@gmail.com>
2020-01-07 15:59:37 +0800

    加入 Redux 全域狀態控制的 Hook 版實作

--- src/views/Practice06/index.js
+++ src/views/Practice06/index.js
@@ -57,3 +57,44 @@ const mapDispatchToProps = dispatch => bindActionCreators({
 export default compose(
   connect(mapStateToProps, mapDispatchToProps)
 )(Practice06)
+
+// ==== Hook version ====
+
+// import React from 'react'
+// import TpSection from '@src/components/TpSection/index'
+// import { useSelector, useDispatch } from 'react-redux'
+// import demoModel from '@src/models/demo'
+
+// const Practice06 = prop => {
+//   const counter = useSelector(state => state.demo.counter)
+//   const dispatch = useDispatch()
+
+//   const handleIncrease = () => {
+//     dispatch(demoModel.action.increaseCounter())
+//   }
+
+//   const handleDecrease = () => {
+//     dispatch(demoModel.action.decreaseCounter())
+//   }
+
+//   return (
+//     <>
+//       <h1> Redux 全域狀態控制 </h1>
+//       <p className='tp-desc'>
+//       熟悉 Redux 全域狀態控制方式，請建立 demoModel (model/demo.js) 並實作一個計數器來控制 demoModel 中的 counter 狀態，
+//       透過 increaseCounter 及 decreaseConter 兩個 action 來控制 reducer 執行 counter 的累加與累減行為，
+//       最後將成果顯示於畫面上。
+//       </p>
+
+//       <TpSection>
+//         <h3>計數器: {counter}</h3>
+//         <button onClick={handleIncrease}>十</button>
+//         <button onClick={handleDecrease}>一</button>
+
+//       </TpSection>
+
+//     </>
+//   )
+// }
+
+// export default Practice06
